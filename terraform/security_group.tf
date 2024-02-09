b4a8fc1169ff0e414fb4beab03f2b972595a562f
oxblixxx <phamust1111@gmail.com>
2024-02-10 00:53:36 +0100

    create sg for public subnet & private subnet

---
+++ terraform/security_group.tf
@@ -0,0 +1,45 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+resource "aws_security_group" "public_subnet" {
+  name        = "public-sg-group"
+  description = "Security group for bastion host in public subnet"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from 
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "public_subnet" {
+  name        = "public-sg-group"
+  description = "Security group for bastion host in public subnet"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Bastion Host cidr_block
+  }
+}
+
+
+
+
+
+
