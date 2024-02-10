bfd917668072f301349388f15b7b7beacb1a40a5
oxblixxx <phamust1111@gmail.com>
2024-02-10 01:00:11 +0100

    added s3 for vpc flow logs

---
+++ terraform/storage.tf
@@ -0,0 +1,33 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "flow_log_bucket" {
+  bucket = "flow_log"
+  force_destroy = false
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
+resource "aws_s3_bucket_acl" "flow_log_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.flow_log_bucket]
+
+  bucket = aws_s3_bucket.flow_log_bucket.id
+  acl    = "private"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_ownership_controls
+resource "aws_s3_bucket_ownership_controls" "flow_log_bucket" {
+  bucket = aws_s3_bucket.flow_log_bucket.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
+resource "aws_s3_bucket_versioning" "flow_log_bucket" {
+  bucket = aws_s3_bucket.flow_log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
