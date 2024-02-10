58940e55bedaa17d4e9b802ac33619b99e4290f1
oxblixxx <phamust1111@gmail.com>
2024-02-11 00:10:18 +0100

    added variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,20 @@
+
+variable "ami" {
+  default = "ami-0c7217cdde317cfec"
+}
+
 variable "bool" {
-  default = true
-  type = bool
+  default = "true"
+  type    = bool
 }
 
 variable "name" {
-    default = ""
+  default = "as"
 }
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+# variable "account_number" {
+# }
