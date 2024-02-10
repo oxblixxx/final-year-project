62155bada4256374532ce207e9010afd2c819a3f
oxblixxx <phamust1111@gmail.com>
2024-02-10 01:12:38 +0100

    add flow logs

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames = var.bool
@@ -10,3 +10,11 @@ resource "aws_vpc" "main" {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/flow_log
+
+resource "aws_flow_log" "example" {
+  log_destination      = aws_s3_bucket.example.arn
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.main.id
+}
