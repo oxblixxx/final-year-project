1ad4e7bb9db502726ff1c732a75a445d97490106
oxblixxx <phamust1111@gmail.com>
2024-02-10 11:20:22 +0100

    modified instance.tf file

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,9 +1,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/subnet
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = ""
+resource "aws_subnet" "public_subnet_main" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = ""
   map_public_ip_on_launch = var.bool
 
   tags = {
@@ -11,12 +11,16 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "private_subnet_main" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = ""
-  private_dns_hostname_type_on_launch = val.bool
+  #ss private_dns_hostname_type_on_launch = true
   tags = {
     Name = "Main"
   }
 }
+
+
+
+
