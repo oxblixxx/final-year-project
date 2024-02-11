9068ab23d7ca3af2ebd9511626aeba417d9823e6
oxblixxx <phamust1111@gmail.com>
2024-02-11 01:05:18 +0100

    added vpc to sg resources

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,8 +1,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 
 resource "aws_security_group" "public_subnet_main" {
-  name        = "public-sg-group"
+  name        = "public-sg-group-1234"
   description = "Security group for bastion host in public subnet"
+  vpc_id = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 22
@@ -29,6 +30,7 @@ resource "aws_security_group" "public_subnet_main" {
 resource "aws_security_group" "private_subnet_main" {
   name        = "public-sg-group"
   description = "Security group for bastion host in public subnet"
+  vpc_id = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 22
@@ -41,6 +43,7 @@ resource "aws_security_group" "private_subnet_main" {
 resource "aws_security_group" "internal_efs" {
   name        = "Internal EFS inbound rule"
   description = "Allow inbound rule from internal_server"
+  vpc_id = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 2096
