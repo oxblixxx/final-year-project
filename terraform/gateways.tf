0e70b8b87917bf7d6a1fad7bfa8c296997afa056
oxblixxx <phamust1111@gmail.com>
2024-02-20 13:07:54 +0100

    modified gateway and routetable

--- terraform/gateways.tf
+++ terraform/gateways.tf
@@ -8,15 +8,15 @@ resource "aws_internet_gateway" "igw" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway_attachment
-resource "aws_internet_gateway_attachment" "main_vpc" {
-  internet_gateway_id = aws_internet_gateway.igw.id
-  vpc_id              = aws_vpc.main_vpc.id
-}
+# resource "aws_internet_gateway_attachment" "main_vpc" {
+#   internet_gateway_id = aws_internet_gateway.igw.id
+#   vpc_id              = aws_vpc.main_vpc.id
+# }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/network_interface
 resource "aws_network_interface" "bastion_host" {
   subnet_id       = aws_subnet.public_subnet_main.id
-  private_ips     = ["192.168.0.20"]
+  private_ips     = ["192.168.1.20"]
   security_groups = [aws_security_group.public_subnet_main.id]
 
   attachment {
@@ -26,25 +26,25 @@ resource "aws_network_interface" "bastion_host" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/network_interface_attachment
-resource "aws_network_interface_attachment" "bastion_host" {
-  instance_id          = aws_instance.bastion_host.id
-  network_interface_id = aws_network_interface.bastion_host.id
-  device_index         = 0
-}
+# resource "aws_network_interface_attachment" "bastion_host" {
+#   instance_id          = aws_instance.bastion_host.id
+#   network_interface_id = aws_network_interface.bastion_host.id
+#   device_index         = 0
+# }
 
-resource "aws_network_interface" "internal_server" {
-  subnet_id       = aws_subnet.private_subnet_main.id
-  private_ips     = ["192.168.0.21"]
-  security_groups = [aws_security_group.private_subnet_main.id]
+# resource "aws_network_interface" "internal_server" {
+#   subnet_id       = aws_subnet.private_subnet_main.id
+#   private_ips     = ["192.168.1.140"]
+#   security_groups = [aws_security_group.private_subnet_main.id]
 
-  attachment {
-    instance     = aws_instance.internal_server.id
-    device_index = 1
-  }
-}
+#   attachment {
+#     instance     = aws_instance.internal_server.id
+#     device_index = 1
+#   }
+# }
 
-resource "aws_network_interface_attachment" "test" {
-  instance_id          = aws_instance.bastion_host.id
-  network_interface_id = aws_network_interface.bastion_host.id
-  device_index         = 0
-}
+# resource "aws_network_interface_attachment" "internal_server" {
+#   instance_id          = aws_instance.internal_server.id
+#   network_interface_id = aws_network_interface.internal_server.id
+#   device_index         = 0
+# }
--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -4,14 +4,15 @@ resource "aws_route_table" "public_subnet_main" {
   vpc_id = aws_vpc.main_vpc.id
 
   route {
-    cidr_block = "192.168.0.0/16"
-    network_interface_id = aws_network_interface.bastion_host.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
     Name = "example"
   }
 }
+
  
 resource "aws_route_table" "private_subnet_main" {
   vpc_id = aws_vpc.main_vpc.id
@@ -28,7 +29,7 @@ resource "aws_route_table" "private_subnet_main" {
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
 resource "aws_route_table_association" "public_subnet_main" {
-  gateway_id = aws_internet_gateway.igw.id
+  subnet_id =  aws_subnet.public_subnet_main.id
   route_table_id = aws_route_table.public_subnet_main.id
 }
 
