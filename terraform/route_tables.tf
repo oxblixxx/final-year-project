4067deadf6365f6b6d2978b2d35c74235688f154
Mutapha Pelumi Babatunde <phamust1111@gmail.com>
2024-03-05 20:17:37 +0000

    uodated nat_gw route

--- terraform/route_tables.tf
+++ terraform/route_tables.tf
@@ -17,10 +17,10 @@ resource "aws_route_table" "public_subnet_main" {
 resource "aws_route_table" "private_subnet_main" {
   vpc_id = aws_vpc.main_vpc.id
 
-#   route {
-#     cidr_block = "192.168.0.0/16"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
 
   tags = {
     Name = "example"
