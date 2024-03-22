db0e72af16d40b7ce4ccdbe325bd83249b6f2c7d
oxblixxx <phamust1111@gmail.com>
2024-03-22 20:38:10 +0100

    fixed tuple issue

--- terraform/infrastructure/route_tables.tf
+++ terraform/infrastructure/route_tables.tf
@@ -13,45 +13,73 @@ resource "aws_route_table" "public_subnet_main" {
   }
 }
 
- 
-resource "aws_route_table" "private_subnet_main" {
+resource "aws_route_table" "private_subnet_main_0" {
   vpc_id = module.vpc.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = module.vpc.natgw_ids
+    gateway_id = module.vpc.natgw_ids[0]
+  }
+}
+
+resource "aws_route_table" "private_subnet_main_1" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.vpc.natgw_ids[0]
+  }
+}
+
+resource "aws_route_table" "private_subnet_main_2" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.vpc.natgw_ids[1]
+  }
+}
+
+resource "aws_route_table" "private_subnet_main_3" {
+  vpc_id = module.vpc.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.vpc.natgw_ids[1]
   }
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
 resource "aws_route_table_association" "public_subnet_main" {
-  subnet_id =  module.vpc.public_subnets[0].id
+  subnet_id =  module.vpc.public_subnets.id
   route_table_id = aws_route_table.public_subnet_main.id
 }
+
+
 
 resource "aws_route_table_association" "private_subnet_directory_1" {
   # Loop through each private subnet and associate it with the route table
   # Access the subnet ID dynamically using the index
-  subnet_id = module.vpc.private_subnets[0].id 
-  route_table_id = aws_route_table.private_subnet_main.id
+  subnet_id = module.vpc.private_subnets[0]
+  route_table_id = aws_route_table.private_subnet_main_0.id
 }
 
 resource "aws_route_table_association" "private_subnet_directory_2" {
   # Loop through each private subnet and associate it with the route table
   # Access the subnet ID dynamically using the index
-  subnet_id = module.vpc.private_subnets[1].id 
-  route_table_id = aws_route_table.private_subnet_main.id
+  subnet_id = module.vpc.private_subnets[1]
+  route_table_id = aws_route_table.private_subnet_main_2.id
 }
 
 resource "aws_route_table_association" "private_subnet_workspace_1" {
   # Access the subnet ID dynamically using the index
-  subnet_id = module.vpc.private_subnets[2].id 
-  route_table_id = aws_route_table.private_subnet_main.id
+  subnet_id = module.vpc.private_subnets[2]
+  route_table_id = aws_route_table.private_subnet_main_1.id
 }
 
 
 resource "aws_route_table_association" "private_subnet_workspace_2" {
   # Access the subnet ID dynamically using the index
-  subnet_id = module.vpc.private_subnets[3].id 
-  route_table_id = aws_route_table.private_subnet_main.id
+  subnet_id = module.vpc.private_subnets[3] 
+  route_table_id = aws_route_table.private_subnet_main_3.id
 }
