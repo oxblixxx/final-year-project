e45b8466b8ac65d87829a1f22c9f60657779fcdc
oxblixxx <phamust1111@gmail.com>
2024-03-22 22:09:34 +0100

    deleted route tbl resource & removed az us-east-1c

--- terraform/infrastructure/vpc.tf
+++ terraform/infrastructure/vpc.tf
@@ -1,4 +1,3 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
 # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -6,24 +5,26 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
   public_subnets  = ["10.0.101.0/24"]
-
   enable_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
+
+
+
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/flow_log
 
 resource "aws_flow_log" "main_vpc" {
   log_destination      = aws_s3_bucket.flow_log_bucket.arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
-  vpc_id               = module.vpc.id
+  vpc_id               = module.vpc.vpc_id
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
