70bf7fd81e7d94f3b20bc821d8e8571db707167b
oxblixxx <phamust1111@gmail.com>
2024-04-02 03:13:54 +0100

    added hcp vaults secret

---
+++ terraform/directory/vault_secrets.tf
@@ -0,0 +1,12 @@
+# https://registry.terraform.io/providers/hashicorp/hcp/latest/docs/data-sources/vault_secrets_app
+# Specify the app in the project (one app_name per data block)
+data "hcp_vault_secrets_app" "directory_secrets" {
+  app_name = "UNILORIN-WORKSPACE"
+# Limit the scope to only one or more secrets in the app
+ secrets= {
+    aws_directory_service_directory_directory_passwd = "version 1" 
+ }
+}
+
+# Replace your existing secret references with
+# data.hcp-vault-secrets_app.aws_app.secret-name
