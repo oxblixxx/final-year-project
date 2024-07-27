90266d0f3361623ebc3f437b7d2bcca3d857c5e6
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2024-07-27 18:49:45 +0200

    Update storage.tf

--- terraform/infrastructure/storage.tf
+++ terraform/infrastructure/storage.tf
@@ -1,6 +1,6 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "flow_log_bucket" {
-  bucket        = "oxblixxxx-flow-log1234"
+  bucket        = "oxblixxxx-flow-log1234567"
   force_destroy = false
   tags = {
     Terraform   = "true"
@@ -8,6 +8,28 @@ resource "aws_s3_bucket" "flow_log_bucket" {
     Project     = "unilorin-workspace"
   }
 }
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  bucket = "oxblixxxx-flow-log123467"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:ListBucket"
+        Resource = "arn:aws:s3:::oxblixxxx-flow-log123467"
+      },
+      {
+        Effect = "Allow"
+        Action = ["s3:GetObject", "s3:PutObject"]
+        Resource = "arn:aws:s3:::oxblixxxx-flow-log123467"
+      }
+    ]
+  })
+}
+
+
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
 resource "aws_s3_bucket_acl" "flow_log_bucket" {
