5331df6dd7d0fc913c6ac5f24eff0e6c88531a1b
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2024-07-29 08:53:21 +0200

    Update main.tf

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# Region for replicated directory service
+provider "aws" {
+  alias  = "secondary"
+  region = "eu-west-1"
+}
+
+
 # Configure statefile to be stored in Backend
 # terraform {
 #   backend "s3" {
